efc58a2053e7cc7ce6dd3c22e254d84a5ecbbcf1
unknown <jrpavoncello@gmail.com>
2016-04-02 21:47:49 -0400

    Initial

---
+++ Source/Escape/EscapeGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "EscapeGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPE_API AEscapeGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
